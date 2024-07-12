cd609ac9027760920aafd886641b3240250024ff
kydrian <kydriangreen34@gmail.com>
2024-07-11 22:17:22 -0400

    added multiple comments

--- index.js
+++ index.js
@@ -35,14 +35,14 @@ inquirer
         },
     ])
 
-    .then((response) => {
+    .then((response) => { // we will take the response from the inquirer prompt as a promise.
         let shape;
         if (response.shape === "circle") {
-            shape = new Circle(response.text, response.textColor, response.shapeColor);
+            shape = new Circle(response.text, response.textColor, response.shapeColor); // if response.shape is strictly equal to circle, then we will create a new Circle object.
         } else if (response.shape === "square") {
-            shape = new Square(response.text, response.textColor, response.shapeColor);
+            shape = new Square(response.text, response.textColor, response.shapeColor); // if response.shape is strictly equal to square, then we will create a new Square object.
         } else if (response.shape === "triangle") {
-            shape = new Triangle(response.text, response.textColor, response.shapeColor);
+            shape = new Triangle(response.text, response.textColor, response.shapeColor); // if response.shape is strictly equal to triangle, then we will create a new Triangle object.
         }
 
         fs.writeFile("logo.svg", `${shape.render()}`, (err) => { // we will write to a file name logo.svg. we will make the file empty, and if there is an error, then we will console.log that error on the console.
